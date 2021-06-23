c9ba22450522d541bd94dbb7a6d491f6d6fb778e
Sree-Vandana <sreevandana.sv@gmail.com>
2021-06-23 01:08:23 -0700

    adding comments and documentation

--- domletter-count.py
+++ domletter-count.py
@@ -2,11 +2,14 @@
 import re
 import sys
 
-
+"""
+@:param  {string}   -   one single word from given input text file.
+@:return {int}      -   This function returns the maximum count of a letter in a given ASCII word.
+"""
 def get_dom_count(word):
     letterCountDict = {}
-    for i in range(len(word)):
-        letterCountDict[word[i]] = (letterCountDict[word[i]] + 1) if letterCountDict.keys().__contains__(word[i]) else 1
+    for w in range(len(word)):
+        letterCountDict[word[w]] = (letterCountDict[word[w]] + 1) if letterCountDict.keys().__contains__(word[w]) else 1
     return max(letterCountDict.values())
 
 
@@ -16,7 +19,7 @@ words = re.split('[\n\s]', stringWords)
 count = 0
 for i in range(len(words)):
     if len(words[i]) > 0:
-        if words[i].isalpha():
+        if words[i].isalpha():  # https://www.geeksforgeeks.org/python-string-isalpha-application/
             count = count + get_dom_count(words[i])
 
 print("Total Count: ", count)
