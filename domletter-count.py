ed3490c60105e13c1010ad5da8bff3a92d2f2d58
Sree-Vandana <sreevandana.sv@gmail.com>
2021-06-23 23:25:25 -0700

    changed input to stdin, removed argument style input

--- domletter-count.py
+++ domletter-count.py
@@ -2,18 +2,21 @@
 import re
 import sys
 
-"""
-@:param  {string}   -   one single word from given input text file.
-@:return {int}      -   This function returns the maximum count of a letter in a given ASCII word.
-"""
+
 def get_dom_count(word):
+    """
+    :param word: one single word from given input text file.
+    :type word: str
+    :return: This function returns the maximum count of a letter in a given ASCII word.
+    :rtype: int
+    """
     letterCountDict = {}
     for w in range(len(word)):
         letterCountDict[word[w]] = (letterCountDict[word[w]] + 1) if letterCountDict.keys().__contains__(word[w]) else 1
     return max(letterCountDict.values())
 
 
-file_name = sys.argv[1]
+file_name = input("Please enter the file name containing ASCII text. (Eg: example.txt)\n")
 stringWords = open(file_name, 'r').read().lower()
 words = re.split('[\n\s]', stringWords)
 count = 0
@@ -22,4 +25,4 @@ for i in range(len(words)):
         if words[i].isalpha():  # https://www.geeksforgeeks.org/python-string-isalpha-application/
             count = count + get_dom_count(words[i])
 
-print("Total Count: ", count)
+print("Total dominant letters Count: ", count)
